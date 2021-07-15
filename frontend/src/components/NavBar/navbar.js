eeb2d80d2a9e833c0407a2907254ba6e8b1eeadd
Vishal Vaddadhi <vvaddadhi@gmail.com>
2021-07-15 11:19:39 -0700

    styling for navbar almost done, need to align it to the right

--- frontend/src/components/NavBar/navbar.js
+++ frontend/src/components/NavBar/navbar.js
@@ -4,8 +4,8 @@ import styles from "./navbar.module.css"
 
 const NavBar = () => {
     return (
-        <Row>
-            <Menu style={{ backgroundColor: "black" }}  mode="horizontal" >
+        <Row justify="right">
+            <Menu style={{ backgroundColor: "black", borderBottomStyle: "none" }}  mode="horizontal" >
                 <Menu.Item key="portfolio" className={styles.itemColors}>Portfolio</Menu.Item>
                 <Menu.Item key="about" className={styles.itemColors}>About</Menu.Item>
                 <Menu.Item key="contact" className={styles.itemColors}>Contact Me</Menu.Item>
